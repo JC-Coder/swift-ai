898d0c7a03582fae3d34f29e29a7afa4b86d8026
JC-Coder <josephchimezie2003@gmail.com>
2022-12-27 16:24:45 +0100

    update

--- server/server.js
+++ server/server.js
@@ -40,7 +40,8 @@ app.post("/", async (req, res) => {
   }
 });
 
+let PORT = process.env.PORT;
 
-app.listen(5000, () => {
-    console.log(`Server is running on port http://localhost:5000`)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
